38ed1ab107acbc0e2d987a07722dce94c376ef5a
Melissa Williams <melissa@brandmeetscopy.com>
2019-05-04 23:27:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
-data.each do |category, info|
-  category.each do |color, name|
-  pigeons << [color][name]
+  data.each do |category, info|
+    info[:color].each_value do |bird_name|
+      pigeons[bird_name]
+    end
   end
-end
 end
