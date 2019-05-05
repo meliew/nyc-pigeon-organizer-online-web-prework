941e4b39d7561cea85169ede3e5c383afe85e255
Melissa Williams <melissa@brandmeetscopy.com>
2019-05-05 01:43:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,20 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  data.each do |attribute_type, attributes|
-    attributes.each do |attribute, array|
-      array.each do |pigeon|
-        if pigeon_list[pigeon]
-          if pigeon_list[pigeon][attribute_type]
-            pigeon_list[pigeon][attribute_type] << attribute.to_s
-          else
-            pigeon_list[pigeon][attribute_type] = [attribute.to_s]
-          end
-        else
-          pigeon_list[pigeon] = {"#{attribute_type}": [attribute.to_s]}
-        end
-      end
-    end
-  end
-  pigeon_list
+  pigeon_list = {}
+  data.each do |attribute_type, attributes|
+    attributes.each do |attribute, array|
+      array.each do |pigeon|
+        if pigeon_list[pigeon]
+          if pigeon_list[pigeon][attribute_type]
+            pigeon_list[pigeon][attribute_type] << attribute.to_s
+          else
+            pigeon_list[pigeon][attribute_type] = [attribute.to_s]
+          end
+        else
+          pigeon_list[pigeon] = {"#{attribute_type}": [attribute.to_s]}
+        end
+      end
+    end
+  end
+  pigeon_list
 end
 end
